a92a67560e93baccd4dfe6fcf39ff9469a61b9a1
acsmit44 <50591795+acsmit44@users.noreply.github.com>
2021-03-21 01:03:13 -0700

    Various changes
    - Added more objects to the GUI
    - Added function to check for error changes
    - Added function to populate GUI text fields with search information

--- grab_data/gui_test.py
+++ grab_data/gui_test.py
@@ -7,71 +7,118 @@ Description:    Main script for processing Jisho search results and otputting
 
 import wx
 import wx.lib.scrolledpanel
+from word_grabber import WordSearch
 
 # Needs to include word, reading, meanings, common or not, and JLPT level
 class SearchFrame(wx.Frame):
+    word_search = WordSearch("")
+
     def __init__(self):
-        super().__init__(parent=None, title='Jisho Grabber', size=(600,600))
+        super().__init__(parent=None, title='Jisho Grabber', size=(700, 700))
 
         vert_sizer = wx.BoxSizer(wx.VERTICAL)
 
         hsizer_1 = wx.BoxSizer(wx.HORIZONTAL)
         search_label = wx.StaticText(self, label='Search term:')
-        search_field = wx.TextCtrl(self)
-        search_button = wx.Button(self, label='Search')
-        add_button = wx.Button(self, label='Add to Deck')
+        self.search_field = wx.TextCtrl(self)
+        self.search_button = wx.Button(self, label='Search')
+        self.search_button.Bind(wx.EVT_BUTTON, self.search_word)
         hsizer_1.Add(search_label, proportion=0, flag=wx.LEFT|wx.TOP|wx.RIGHT, border=8)
-        hsizer_1.Add(search_field, proportion=1, flag=wx.TOP, border=8)
-        hsizer_1.Add(search_button, proportion=0, flag=wx.TOP, border=8)
-        hsizer_1.Add(add_button, proportion=0, flag=wx.RIGHT|wx.TOP, border=8)
+        hsizer_1.Add(self.search_field, proportion=1, flag=wx.TOP, border=8)
+        hsizer_1.Add(self.search_button, proportion=0, flag=wx.TOP|wx.RIGHT, border=8)
         vert_sizer.Add(hsizer_1, flag=wx.EXPAND)
 
         hsizer_2 = wx.BoxSizer(wx.HORIZONTAL)
-        back_button = wx.Button(self, label='Previous result')
-        next_button = wx.Button(self, label='Next result')
-        hsizer_2.Add(back_button, proportion=1, flag=wx.LEFT, border=8)
-        hsizer_2.Add(next_button, proportion=1, flag=wx.RIGHT, border=8)
+        self.back_button = wx.Button(self, label='Previous result')
+        self.next_button = wx.Button(self, label='Next result')
+        hsizer_2.Add(self.back_button, proportion=1, flag=wx.LEFT, border=8)
+        hsizer_2.Add(self.next_button, proportion=1, flag=wx.RIGHT, border=8)
         vert_sizer.Add(hsizer_2, flag=wx.EXPAND)
 
-        vert_sizer.Add(wx.StaticLine(self, style=wx.LI_HORIZONTAL), 0, wx.ALL | wx.EXPAND, 10)
+        self.search_error = wx.StaticText(self, label='No error.')
+        self.search_error.SetForegroundColour((0,255,0))
+        font = wx.Font(18, wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
+        self.search_error.SetFont(font)
+        vert_sizer.Add(self.search_error, proportion=0, flag=wx.TOP|wx.LEFT|wx.RIGHT| \
+                       wx.ALIGN_CENTER_HORIZONTAL, border=8)
+
+        vert_sizer.Add(wx.StaticLine(self, style=wx.LI_HORIZONTAL), proportion=0, \
+                       flag=wx.ALL|wx.EXPAND, border=8)
 
         hsizer_3 = wx.BoxSizer(wx.HORIZONTAL)
 
         subvert_1 = wx.BoxSizer(wx.VERTICAL)
         word_label = wx.StaticText(self, label='Word:', size=(-1, 20))
         reading_label = wx.StaticText(self, label='Reading:', size=(-1, 20))
-        subvert_1.Add(word_label, 0, flag=wx.LEFT, border=8)
-        subvert_1.Add(reading_label, 0, flag=wx.LEFT, border=8)
+        subvert_1.Add(word_label, 0, flag=wx.LEFT|wx.RIGHT, border=8)
+        subvert_1.Add(reading_label, 0, flag=wx.LEFT|wx.RIGHT, border=8)
         hsizer_3.Add(subvert_1, proportion=0)
 
         subvert_2 = wx.BoxSizer(wx.VERTICAL)
-        word_reading = wx.TextCtrl(self, style=wx.TE_READONLY, size=(-1, 20))
-        word_result = wx.TextCtrl(self, style=wx.TE_READONLY, size=(-1, 20))
-        subvert_2.Add(word_reading, 0, flag=wx.EXPAND, border=8)
-        subvert_2.Add(word_result, 0, flag=wx.EXPAND, border=8)
+        self.word_result = wx.TextCtrl(self, style=wx.TE_READONLY, size=(-1, 20))
+        self.word_reading = wx.TextCtrl(self, style=wx.TE_READONLY, size=(-1, 20))
+        subvert_2.Add(self.word_result, 0, flag=wx.EXPAND, border=8)
+        subvert_2.Add(self.word_reading, 0, flag=wx.EXPAND, border=8)
         hsizer_3.Add(subvert_2, proportion=1, flag=wx.EXPAND)
 
         subvert_3 = wx.BoxSizer(wx.VERTICAL)
         jlpt_label = wx.StaticText(self, label='JLPT Level:', size=(-1, 20))
         common_label = wx.StaticText(self, label='Common word?:', size=(-1, 20))
-        subvert_3.Add(jlpt_label, 0, border=8)
-        subvert_3.Add(common_label, 0, border=8)
+        subvert_3.Add(jlpt_label, 0, flag=wx.LEFT|wx.RIGHT, border=8)
+        subvert_3.Add(common_label, 0, flag=wx.LEFT|wx.RIGHT, border=8)
         hsizer_3.Add(subvert_3, proportion=0)
 
         subvert_4 = wx.BoxSizer(wx.VERTICAL)
-        jlpt_level = wx.TextCtrl(self, style=wx.TE_READONLY, size=(-1, 20))
-        common_word = wx.TextCtrl(self, style=wx.TE_READONLY, size=(-1, 20))
-        subvert_4.Add(jlpt_level, 0, flag=wx.RIGHT|wx.EXPAND, border=8)
-        subvert_4.Add(common_word, 0, flag=wx.RIGHT|wx.EXPAND, border=8)
+        self.jlpt_level = wx.TextCtrl(self, style=wx.TE_READONLY, size=(-1, 20))
+        self.common_word = wx.TextCtrl(self, style=wx.TE_READONLY, size=(-1, 20))
+        subvert_4.Add(self.jlpt_level, 0, flag=wx.RIGHT|wx.EXPAND, border=8)
+        subvert_4.Add(self.common_word, 0, flag=wx.RIGHT|wx.EXPAND, border=8)
         hsizer_3.Add(subvert_4, proportion=1, flag=wx.EXPAND)
 
         vert_sizer.Add(hsizer_3, flag=wx.EXPAND)
 
+        vert_sizer.Add(wx.StaticText(self, label='Meanings:'), 0, \
+                       flag=wx.LEFT, border=8)
+
+        hsizer_4 = wx.BoxSizer(wx.HORIZONTAL)
+        self.meanings_body = wx.TextCtrl(self, style=wx.TE_MULTILINE|wx.TE_READONLY)
+        hsizer_4.Add(self.meanings_body, proportion=1, flag=wx.EXPAND)
+        vert_sizer.Add(hsizer_4, proportion=1, flag=wx.LEFT|wx.RIGHT|wx.EXPAND, \
+                       border=8)
+
+        vert_sizer.Add(wx.StaticLine(self, style=wx.LI_HORIZONTAL), 0, \
+                       wx.ALL|wx.EXPAND, 10)
+
+        hsizer_5 = wx.BoxSizer(wx.HORIZONTAL)
+        self.add_button = wx.Button(self, label='Add to Deck')
+        hsizer_5.Add(self.add_button, proportion=1, flag=wx.RIGHT|wx.LEFT|wx.BOTTOM, border=8)
+        vert_sizer.Add(hsizer_5, flag=wx.EXPAND)
+
         self.SetSizer(vert_sizer)
         self.Show()
+    
+    def check_error(self):
+        self.search_error.SetLabel(self.word_search.error_msg)
+        if self.word_search.error_msg == "No error.":
+            self.search_error.SetForegroundColour((0,255,0))
+            self.set_fields()
+        else:
+            self.search_error.SetForegroundColour((255,0,0))
+        self.search_error.GetContainingSizer().Layout()
+        return bool(self.word_search.error_msg == "No error.")
+
+    def set_fields(self):
+        self.word_result.SetLabel(self.word_search.word_dict['Word'])
+        self.word_reading.SetLabel(self.word_search.word_dict['Reading'])
+        self.jlpt_level.SetLabel(self.word_search.word_dict['JLPT'])
+        self.common_word.SetLabel(self.word_search.word_dict['Common'])
+
+    def search_word(self, event):
+        self.word_search = WordSearch(self.search_field.GetValue())
+        self.check_error()
 
 if __name__ == '__main__':
     app = wx.App()
     frame = SearchFrame()
     app.MainLoop()
-    print("Done")
+    print("Program closed.")
