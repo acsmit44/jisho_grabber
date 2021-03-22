24179f2dd5675ce3a3d23d102fe8dcd52291b42f
acsmit44 <50591795+acsmit44@users.noreply.github.com>
2021-03-21 23:18:11 -0700

    Anki deck now written to relative path

--- grab_data/main.py
+++ grab_data/main.py
@@ -15,19 +15,24 @@ from ankify import jisho_vocab, jisho_deck
 
 if __name__ == '__main__':
     jsonpath = os.path.join(os.getcwd(), 'grab_data', 'vocab_words.json')
+    ankipath = os.path.join(os.getcwd(), 'grab_data', 'output.apkg')
 
+    # create json if none exists
     if not os.path.exists(jsonpath):
         print("Creating vocab_words.json...")
         with open(jsonpath, 'w') as outfile:
             json.dump([], outfile, indent=2)
 
+    # load existing vocab words
     with open(jsonpath, 'r') as infile:
         all_vocab = json.load(infile)
 
+    # run gui
     app = wx.App()
     frame = SearchFrame()
     app.MainLoop()
 
+    # create new notes for all words the user selected
     for new_note_fields in frame.fields_list:
         new_note = genanki.Note(
             model=jisho_vocab,
@@ -35,7 +40,7 @@ if __name__ == '__main__':
         )
         jisho_deck.add_note(new_note)
     if len(frame.fields_list) > 0:
-        genanki.Package(jisho_deck).write_to_file('output.apkg')
+        genanki.Package(jisho_deck).write_to_file(ankipath)
         all_vocab.extend(frame.fields_list)
 
     with open(jsonpath, 'w') as outfile:
