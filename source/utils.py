d94043023c0a21f9b9fb6a2c71dd6cf5185325cc
acsmit44 <50591795+acsmit44@users.noreply.github.com>
2021-03-27 12:40:37 -0700

    Added function to get only okurigana or kana

--- source/utils.py
+++ source/utils.py
@@ -4,6 +4,7 @@ Project:        Jisho grabber
 Description:    Various helper functions to make the processing of jisho search
                 results easier or more readable.
 '''
+import re
 
 # Checks if a character is Hiragana, returning true if yes and false otherwise
 def isHiragana(c):
@@ -17,3 +18,16 @@ def isKatakana(c):
 # false otherwise
 def isKana(c):
     return True if isHiragana(c) or isKatakana(c) else False
+
+# Returns only kana from a word
+def kana(word):
+    allKana = ""
+    for jpnchar in word:
+        if isKana(jpnchar):
+            allKana += jpnchar
+    return allKana
+
+# Returns only okurigana from a word
+def kanji(word):
+    # remove all strings within square brackets, and remove the square brackets
+    return re.sub("[\[].*?[\]]", "", word).replace(" ", "")
